5d21b83a42182c7ace3e93120b6cb503ead96f70
Joseph Elias <jrelias@outlook.com>
2022-12-12 17:37:46 -0500

    created projects section and component

---
+++ components/Projects.jsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+// import octohome from '../public/assets/projects/octohome.png';
+// import octohome from '../public/assets/octohome.PNG';
+import octohome from '../public/assets/projects/octohome.PNG';
+import ProjectItem from './ProjectItem';
+// import joe from '../public/assets/Joe.JPG';
+import pizzapath from '../public/assets/projects/pizzapaths.JPEG';
+
+const Projects = () => {
+  return (
+    <div className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I've Built</h2>
+
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='Octo Game'
+            backgroundImg={octohome}
+            projectUrl='/octogame'
+          />
+          <ProjectItem
+            title='Pizza Paths'
+            backgroundImg={pizzapath}
+            projectUrl='/pizzapaths'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,10 +8,13 @@ const Skills = () => {
     'react',
     'nextjs',
     'node',
+    'postgresql',
+    'redux',
     'html',
     'css',
     'mongo',
     'tailwind',
+    'firebase',
     'github',
   ];
   return (
@@ -20,7 +23,7 @@ const Skills = () => {
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Skills
         </p>
-        <h2>Technologies I've worked with</h2>
+        <h2 className='py-4'>Technologies I've worked with</h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
           {skills.map((skill) => {
             return <Skillbanner skillName={skill} key={skill} />;
