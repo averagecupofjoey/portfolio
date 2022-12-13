ea087871cca3d3bd03d1bfaad36fbfc55b3829d4
Joseph Elias <jrelias@outlook.com>
2022-12-13 12:49:57 -0500

    built contact form

---
+++ components/Contact.jsx
@@ -0,0 +1,110 @@
+import Image from 'next/image';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import contact from '../public/assets/contact.jpg';
+
+const Contact = () => {
+  return (
+    <div className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+        <p className='text0xl tracking-widest uppercase text-[#5651e5]'>
+          Contact
+        </p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl py-4'>
+            <div className='lg:p-4 h-full'>
+              <div>
+                <Image
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src={contact}
+                  alt='contact image'
+                  height={500}
+                />
+              </div>
+              <div>
+                <h2 className='py-2'>Joseph Elias</h2>
+                <p>Fullstack Developer</p>
+                <p className='py-4'>
+                  I'm currently looking for a full-time position. Contact me if
+                  you have a role that I'd be a good fit for!
+                </p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect With Me</p>
+                <div className='flex items-center justify-between py-4'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointe hover:scale-110 ease-in duration-300'>
+                    <FaLinkedin />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointe hover:scale-110 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointe hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointe hover:scale-110 ease-in duration-300'>
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* right side */}
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+              <form>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Name</label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                    />
+                  </div>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>
+                      Phone Number
+                    </label>
+                    <input
+                      className='border-2 rounded-lg p-3 flex border-gray-300'
+                      type='text'
+                    />
+                  </div>
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Email</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='email'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Subject</label>
+                  <input
+                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    type='text'
+                  />
+                </div>
+                <div className='flex flex-col py-2'>
+                  <label className='uppercase text-sm py-2'>Message</label>
+                  <textarea
+                    className='border-2 rounded-lg p-3 border-gray-300'
+                    rows='10'
+                  ></textarea>
+                </div>
+                <button className='w-full p-4 mt-4 text-gray-100'>
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
